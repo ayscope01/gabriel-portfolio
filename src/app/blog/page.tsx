41bc7448361735048ec56de56cd91281f5017a38
Fadare Gabriel <gabriel704910@gmail.com>
2026-08-14 14:52:27 +0100

    Fix blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,3 @@
 export default function Blog() {
   return null;
-}        <Posts range={[2, 3]} columns="2" thumbnail direction="column" />
-        <Mailchimp marginBottom="l" />
-        <Heading as="h2" variant="heading-strong-xl" marginLeft="l">
-          Earlier posts
-        </Heading>
-        <Posts range={[4]} columns="2" />
-      </Column>
-    </Column>
-  );
 }
